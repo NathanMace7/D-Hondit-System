880f23e15746ad9b6123cd6c5d8459cb230a98b9
Nathan Mace <19705377@students.lincoln.ac.uk>
2021-07-28 13:12:54 +0100

    Add project files.

---
+++ D'Hondit System/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace D_Hondit_System
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
